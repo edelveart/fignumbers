9715d55dbb6d41b5ac1e5012b6ff095323c2e33d
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-03 18:19:29 -0500

    feat(plane-fignumbers/context): add PlaneFigNumbers context class responsible for selecting strategy

---
+++ src/plane-fignumbers/context/PlaneGeneratorContext.ts
@@ -0,0 +1,22 @@
+import { type PlaneConfigMap, planeMapStrategy } from "../mapped-types/PlaneMappedTypes.js";
+import { CONFIG_CONSTANTS } from "../../core/types/configConstants.js";
+import { Validator } from "../../core/validators/Validator.js";
+
+export class PlaneFigNumbers {
+  static generate<KMap extends keyof PlaneConfigMap>(
+    strategyName: KMap,
+    config: PlaneConfigMap[KMap],
+  ): Generator<number> {
+    if (CONFIG_CONSTANTS.step in config) {
+      Validator.validateStep(config.step);
+    }
+    if (CONFIG_CONSTANTS.start in config) {
+      Validator.validateStart(config.start);
+    }
+    if (CONFIG_CONSTANTS.m in config) {
+      Validator.validateMFacets(config.m);
+    }
+
+    return planeMapStrategy[strategyName].generate(config);
+  }
+}
