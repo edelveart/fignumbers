1e64c1a558435f9563834de6d9be5dca3e55d136
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-03 18:04:38 -0500

    feat(core/validators): move validator and error messages to core module

---
+++ src/core/validators/Validator.ts
@@ -0,0 +1,27 @@
+import { mFacetsErrorMessage, startErrorMessage, stepErrorMessage } from "./errorMessages.js";
+
+export class Validator {
+  static validateStep(step: number | undefined) {
+    if (step !== undefined) {
+      if (!Number.isInteger(step) || step <= 0) {
+        throw new Error(stepErrorMessage);
+      }
+    }
+  }
+
+  static validateStart(start: number | undefined) {
+    if (start !== undefined) {
+      if (!Number.isInteger(start)) {
+        throw new Error(startErrorMessage);
+      }
+    }
+  }
+
+  static validateMFacets(m: number | undefined) {
+    if (m !== undefined) {
+      if (!Number.isInteger(m) || m < 3) {
+        throw new Error(mFacetsErrorMessage);
+      }
+    }
+  }
+}
---
+++ src/core/validators/errorMessages.ts
@@ -0,0 +1,3 @@
+export const stepErrorMessage = "Parameter 'step' must be an integer greater than or equal to 1";
+export const startErrorMessage = "Parameter 'start' must be an integer";
+export const mFacetsErrorMessage = "Parameter 'm' must be an integer greater than or equal to 3";
