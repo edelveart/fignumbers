9320acfda02e36cdd5dfa3a1f9196418c437cce2
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-04 19:14:16 -0500

    feat(space-fignumbers/strategies): add 'start' parameter to PyramidalGenerator class

--- src/space-fignumbers/strategies/PyramidalGenerator.ts
+++ src/space-fignumbers/strategies/PyramidalGenerator.ts
@@ -1,11 +1,11 @@
-import type { MfacetsStepParameterConfig } from "../../core/types/ConfigTypes.js";
+import type { StartStepMfacetsParameterConfig } from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
 
-export type PyramidalConfig = MfacetsStepParameterConfig;
+export type PyramidalConfig = StartStepMfacetsParameterConfig;
 
 export class PyramidalGenerator implements INumberGeneratorStrategy<PyramidalConfig> {
-  *generate({ step = 1, m = 3 }: PyramidalConfig): Generator<number> {
-    let delta = 1;
+  *generate({ start = 1, step = 1, m = 3 }: PyramidalConfig): Generator<number> {
+    let delta = start;
     while (true) {
       yield (delta * (delta + 1) * ((m - 2) * delta - m + 5)) / 6;
       delta += step;
