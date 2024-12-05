845c0248b8d15b3bb430bb9d1800a78ccee3fd5f
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 15:41:22 -0500

    fix(core/validators): update Validator class for handling large numbers in parameters

--- src/core/validators/Validator.ts
+++ src/core/validators/Validator.ts
@@ -1,26 +1,32 @@
 import { ERROR_MESSAGE } from "./errorMessages.js";
 
 export class Validator {
-  static validateStep(step: number | undefined) {
+  static validateStep(step: bigint | undefined) {
     if (step !== undefined) {
-      if (!Number.isInteger(step) || step <= 0) {
+      if (typeof step !== "bigint") {
+        throw new Error(ERROR_MESSAGE.stepErrorMessage);
+      }
+      if (step <= 0n) {
         throw new Error(ERROR_MESSAGE.stepErrorMessage);
       }
     }
   }
 
-  static validateStart(start: number | undefined) {
+  static validateStart(start: bigint | undefined) {
     if (start !== undefined) {
-      if (!Number.isInteger(start)) {
-        throw new Error(ERROR_MESSAGE.startErrorMessage);
+      if (typeof start !== "bigint") {
+        throw new Error(ERROR_MESSAGE.stepErrorMessage);
       }
     }
   }
 
-  static validateMFacets(m: number | undefined) {
+  static validateMFacets(m: bigint | undefined) {
     if (m !== undefined) {
-      if (!Number.isInteger(m) || m < 3) {
+      if (typeof m !== "bigint") {
         throw new Error(ERROR_MESSAGE.mFacetsErrorMessage);
+      }
+      if (m <= 2n) {
+        throw new Error(ERROR_MESSAGE.stepErrorMessage);
       }
     }
   }
