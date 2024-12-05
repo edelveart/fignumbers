e5689ac526aa389dbfbbc9b48efa779a7f87d30a
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 16:00:04 -0500

    refactor(plane-fignumbers/strategies/Pronic): use BigInt instead of number for handling large values

--- src/plane-fignumbers/strategies/PronicGenerator.ts
+++ src/plane-fignumbers/strategies/PronicGenerator.ts
@@ -4,10 +4,10 @@ import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGene
 export type PronicConfig = StepParameterConfig;
 
 export class PronicGenerator implements INumberGeneratorStrategy<PronicConfig> {
-  *generate({ step = 1 }: PronicConfig): Generator<number> {
-    let delta = 1;
+  *generate({ step = 1n }: PronicConfig): Generator<bigint> {
+    let delta = 1n;
     while (true) {
-      yield delta * (delta + 1);
+      yield delta * (delta + 1n);
       delta += step;
     }
   }
