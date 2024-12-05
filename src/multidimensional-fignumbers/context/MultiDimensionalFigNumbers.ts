df36a461bb1dc943d8048af89ec13d9f7854d86f
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 01:20:33 -0500

    feat(multidimensional-fignumbers/context): create MultiDimensionalFigNumbers class

---
+++ src/multidimensional-fignumbers/context/MultiDimensionalFigNumbers.ts
@@ -0,0 +1,25 @@
+import { CONFIG_CONSTANTS } from "../../core/types/configConstants.js";
+import { Validator } from "../../core/validators/Validator.js";
+import {
+  multiDimensionalMapStrategy,
+  type MultiDimensionalConfigMap,
+} from "../mapped-types/MultidimensionalMappedTypes.js";
+
+export class MultiDimensionalFigNumbers {
+  static generate<KMap extends keyof MultiDimensionalConfigMap>(
+    strategyName: KMap,
+    config: MultiDimensionalConfigMap[KMap],
+  ): Generator<number> {
+    if (CONFIG_CONSTANTS.step in config) {
+      Validator.validateStep(config.step);
+    }
+    if (CONFIG_CONSTANTS.start in config) {
+      Validator.validateStart(config.start);
+    }
+    if (CONFIG_CONSTANTS.m in config) {
+      Validator.validateMFacets(config.m);
+    }
+
+    return multiDimensionalMapStrategy[strategyName].generate(config);
+  }
+}
