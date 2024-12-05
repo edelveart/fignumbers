6ea40453e418079c7b87350108437e7df7ac0674
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 15:59:27 -0500

    refactor(plane-fignumbers/strategies/PolygramGenerator): use BigInt instead of number for handling large values

--- src/plane-fignumbers/strategies/PolygramGenerator.ts
+++ src/plane-fignumbers/strategies/PolygramGenerator.ts
@@ -4,10 +4,10 @@ import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGene
 export type PolygramConfig = MfacetsStepParameterConfig;
 
 export class PolygramGenerator implements INumberGeneratorStrategy<PolygramConfig> {
-  *generate({ step = 1, m = 3 }: PolygramConfig): Generator<number> {
-    let delta = 1;
+  *generate({ step = 1n, m = 3n }: PolygramConfig): Generator<bigint> {
+    let delta = 1n;
     while (true) {
-      yield m * delta ** 2 - m * delta + 1;
+      yield m * delta ** 2n - m * delta + 1n;
       delta += step;
     }
   }
