0c782a2df6f061657c14ae85588fbd4cc4032fa3
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 18:25:27 -0500

    test(space-fignumbers/integration): force type error in test cases by using 'any | unknown' for non-bigint values

--- tests/space-fignumbers/integration/SpaceFigNumbersErrorValidation.test.ts
+++ tests/space-fignumbers/integration/SpaceFigNumbersErrorValidation.test.ts
@@ -4,8 +4,8 @@ import { ERROR_MESSAGE } from "../../../src/core/validators/errorMessages";
 
 describe("Space Figurate Numbers Error Validation", () => {
   it("should throw an error if 'step' parameter is not an integer or is less than 1", () => {
-    const invalidSteps = [0.5, -3, 7.43];
-    invalidSteps.forEach((step) => {
+    const invalidSteps = [0.5, -3, 7.43, 0n];
+    invalidSteps.forEach((step: unknown | any) => {
       expect(() => SpaceFigNumbers.generate("pyramidal", { step })).toThrowError(
         ERROR_MESSAGE.stepErrorMessage,
       );
@@ -13,8 +13,8 @@ describe("Space Figurate Numbers Error Validation", () => {
   });
 
   it("should throw an error if 'start' parameter is not integer", () => {
-    const invalidStart = [0.3, -3.4, 6.81];
-    invalidStart.forEach((start) => {
+    const invalidStart = [0.3, -3.4, 6.81, -13];
+    invalidStart.forEach((start: unknown | any) => {
       expect(() => SpaceFigNumbers.generate("pyramidal", { start })).toThrowError(
         ERROR_MESSAGE.startErrorMessage,
       );
@@ -22,8 +22,8 @@ describe("Space Figurate Numbers Error Validation", () => {
   });
 
   it("should throw an error if 'm' parameter is not an integer or is less than 3", () => {
-    const invalidMFacets = [0.5, 2, -1, 9.3];
-    invalidMFacets.forEach((m) => {
+    const invalidMFacets = [0.5, 2, -1, 9.3, 2n];
+    invalidMFacets.forEach((m: unknown | any) => {
       expect(() => SpaceFigNumbers.generate("pyramidal", { m })).toThrowError(
         ERROR_MESSAGE.mFacetsErrorMessage,
       );
