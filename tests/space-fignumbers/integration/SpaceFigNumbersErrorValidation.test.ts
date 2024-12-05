fc5ae12a5c9107692c8c80456835e4c4791f0d4c
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-04 23:43:36 -0500

    test(space-fignumbers/integration): add error validation tests for SpaceFigNumbers

---
+++ tests/space-fignumbers/integration/SpaceFigNumbersErrorValidation.test.ts
@@ -0,0 +1,32 @@
+import { expect, it, describe } from "vitest";
+import { SpaceFigNumbers } from "../../../src/index";
+import { ERROR_MESSAGE } from "../../../src/core/validators/errorMessages";
+
+describe("Space Figurate Numbers Error Validation", () => {
+  it("should throw an error if 'step' parameter is not an integer or is less than 1", () => {
+    const invalidSteps = [0.5, -3, 7.43];
+    invalidSteps.forEach((step) => {
+      expect(() => SpaceFigNumbers.generate("pyramidal", { step })).toThrowError(
+        ERROR_MESSAGE.stepErrorMessage,
+      );
+    });
+  });
+
+  it("should throw an error if 'start' parameter is not integer", () => {
+    const invalidStart = [0.3, -3.4, 6.81];
+    invalidStart.forEach((start) => {
+      expect(() => SpaceFigNumbers.generate("pyramidal", { start })).toThrowError(
+        ERROR_MESSAGE.startErrorMessage,
+      );
+    });
+  });
+
+  it("should throw an error if 'm' parameter is not an integer or is less than 3", () => {
+    const invalidMFacets = [0.5, 2, -1, 9.3];
+    invalidMFacets.forEach((m) => {
+      expect(() => SpaceFigNumbers.generate("pyramidal", { m })).toThrowError(
+        ERROR_MESSAGE.mFacetsErrorMessage,
+      );
+    });
+  });
+});
