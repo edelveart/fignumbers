dcd238518a69b8d8b76ceb5acc50e75d84291f3e
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 16:57:42 -0500

    refactor(tests/utils): refactor and rename main helper function to simplify parameters and reduce redundancy

--- tests/utils/helperGeneratorContext.ts
+++ tests/utils/helperGeneratorContext.ts
@@ -1,15 +1,23 @@
-export const createArrOfFigNumbers = (
-  lengthOfSequence: bigint,
-  numberGenerator: Generator<bigint>,
-) => {
-  const numberSequence: bigint[] = [];
-  for (let i = 0; i < lengthOfSequence; i++) {
-    const { value } = numberGenerator.next();
-    numberSequence.push(value);
-  }
-  return numberSequence;
-};
+import { expect } from "vitest";
 
 export const convertNumbersToBigInt = (listOfFigNumbers: number[]) => {
   return [...listOfFigNumbers].map((num) => BigInt(num));
+};
+
+export const compareExpectToEqual = (
+  inputFigGenerator: Generator<bigint>,
+  expectedResultArr: number[] | bigint[],
+) => {
+  const receivedGeneratorArr: bigint[] = [];
+
+  for (let i = 0; i < expectedResultArr.length; i++) {
+    const { value } = inputFigGenerator.next();
+    receivedGeneratorArr.push(value);
+  }
+
+  if (expectedResultArr.every((num) => typeof num === "number")) {
+    return expect(receivedGeneratorArr).toEqual(convertNumbersToBigInt(expectedResultArr));
+  } else {
+    return expect(receivedGeneratorArr).toEqual(expectedResultArr);
+  }
 };
