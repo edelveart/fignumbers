47b16eefeec1dd4a5528e6f8e02bab1b4f009baa
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 17:35:46 -0500

    test(plane-fignumbers/integration): force type error in test cases by using 'any | unknown' for non-bigint values

--- tests/plane-fignumbers/integration/PlaneFigNumbersErrorValidation.test.ts
+++ tests/plane-fignumbers/integration/PlaneFigNumbersErrorValidation.test.ts
@@ -3,27 +3,27 @@ import { PlaneFigNumbers } from "../../../src/index";
 import { ERROR_MESSAGE } from "../../../src/core/validators/errorMessages";
 
 describe("Plane Figurate Numbers Error Validation", () => {
-  it("should throw an error if 'step' parameter is not an integer or is less than 1", () => {
-    const invalidSteps = [0.5, -3, 7.43];
-    invalidSteps.forEach((step) => {
+  it("should throw an error if 'step' parameter is not a biginteger or is less than 1n", () => {
+    const invalidSteps = [-4n, -3n, 0.5];
+    invalidSteps.forEach((step: unknown | any) => {
       expect(() => PlaneFigNumbers.generate("polygonal", { step })).toThrowError(
         ERROR_MESSAGE.stepErrorMessage,
       );
     });
   });
 
-  it("should throw an error if 'start' parameter is not integer", () => {
-    const invalidStart = [0.3, -3.4, 6.81];
-    invalidStart.forEach((start) => {
+  it("should throw an error if 'start' parameter is not biginteger", () => {
+    const invalidStart = [0.3, 0.12, 6.85, 0.5];
+    invalidStart.forEach((start: unknown | any) => {
       expect(() => PlaneFigNumbers.generate("polygonal", { start })).toThrowError(
         ERROR_MESSAGE.startErrorMessage,
       );
     });
   });
 
-  it("should throw an error if 'm' parameter is not an integer or is less than 3", () => {
-    const invalidMFacets = [0.5, 2, -1, 9.3];
-    invalidMFacets.forEach((m) => {
+  it("should throw an error if 'm' parameter is not an biginteger or is less than 3n", () => {
+    const invalidMFacets = [-5n, -48n, -0n, 4.35];
+    invalidMFacets.forEach((m: unknown | any) => {
       expect(() => PlaneFigNumbers.generate("polygonal", { m })).toThrowError(
         ERROR_MESSAGE.mFacetsErrorMessage,
       );
