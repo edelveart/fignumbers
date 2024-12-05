70a68010c46614fa5af952203861e2098e72eb0f
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 01:59:36 -0500

    chore(src/index): update export to  MultiDimensionalFigNumbers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./plane-fignumbers/context/PlaneFigNumbers.js";
 export * from "./space-fignumbers/context/SpaceFigNumbers.js";
-export * from "./multidimensional-fignumbers/context/multiDimensionalFigNumbers.js";
+export * from "./multidimensional-fignumbers/context/MultiDimensionalFigNumbers.js";
