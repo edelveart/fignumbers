51578a9b791f3e064e2e76774aed28349815fd1a
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-05 19:08:27 -0500

    test(multidimensional-fignumbers/integration): force type error in test cases by using 'any | unknown' for non-bigint values

--- tests/multidimensional-fignumbers/integration/MultiDimensionalFigNumbersErrorValidation.test.ts
+++ tests/multidimensional-fignumbers/integration/MultiDimensionalFigNumbersErrorValidation.test.ts
@@ -3,27 +3,27 @@ import { MultiDimensionalFigNumbers } from "../../../src/index";
 import { ERROR_MESSAGE } from "../../../src/core/validators/errorMessages";
 
 describe("MultiDimensional Figurate Numbers Error Validation", () => {
-  it("should throw an error if 'step' parameter is not an integer or is less than 1", () => {
-    const invalidSteps = [0.5, -3, 7.43];
-    invalidSteps.forEach((step) => {
+  it("should throw an error if the 'm' parameter is not a BigInt or is less than 1n", () => {
+    const invalidSteps = [0.5, -3, 7.43, -0n, -32n];
+    invalidSteps.forEach((step: unknown | any) => {
       expect(() => MultiDimensionalFigNumbers.generate("biquadratic", { step })).toThrowError(
         ERROR_MESSAGE.stepErrorMessage,
       );
     });
   });
 
-  it("should throw an error if 'start' parameter is not integer", () => {
-    const invalidStart = [0.3, -3.4, 6.81];
-    invalidStart.forEach((start) => {
+  it("should throw an error if the 'start' parameter is not a BigInt", () => {
+    const invalidStart = [0.3, -3.4, 6.81, 39.4, -129, 2];
+    invalidStart.forEach((start: unknown | any) => {
       expect(() =>
         MultiDimensionalFigNumbers.generate("kDimensionalHyperCube", { start }),
       ).toThrowError(ERROR_MESSAGE.startErrorMessage);
     });
   });
 
-  it("should throw an error if 'm' parameter is not an integer or is less than 3", () => {
-    const invalidMFacets = [0.5, 2, -1, 9.3];
-    invalidMFacets.forEach((m) => {
+  it("should throw an error if the 'm' parameter is not a BigInt or is less than 3n", () => {
+    const invalidMFacets = [-9n, -129n, -0n, 6.75];
+    invalidMFacets.forEach((m: unknown | any) => {
       expect(() =>
         MultiDimensionalFigNumbers.generate("fourDimensionalMgonalPyramidal", { m }),
       ).toThrowError(ERROR_MESSAGE.mFacetsErrorMessage);
