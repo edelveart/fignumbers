34807bbf59b2e48874e60bee5e2aebf7a35c20a1
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-06 11:42:31 -0500

    feat(plane-fignumbers/strategies): add PoliteGenerator class

--- src/plane-fignumbers/strategies/PoliteGenerator.ts
+++ src/plane-fignumbers/strategies/PoliteGenerator.ts
@@ -1,13 +1,14 @@
 import type { StepParameterConfig } from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
+import { log2BigInt } from "../utils/helpersPlaneFigNumbers.js";
 
 export type PoliteConfig = StepParameterConfig;
 
 export class PoliteGenerator implements INumberGeneratorStrategy<PoliteConfig> {
-  *generate({ step = 1 }: PoliteConfig): Generator<number> {
-    let delta = 1;
+  *generate({ step = 1n }: PoliteConfig): Generator<bigint> {
+    let delta = 1n;
     while (true) {
-      yield delta + Math.floor(Math.log2(delta + Math.log2(delta)));
+      yield delta + log2BigInt(delta + log2BigInt(delta));
       delta += step;
     }
   }
