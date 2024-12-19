7c379b0be6e7779cc77facc016dc7d955125a47f
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-19 15:57:20 -0500

    feat(multidimensional-fignumbers/strategies): add KDimensionalMgonalPyramidal Generator class

---
+++ src/multidimensional-fignumbers/strategies/KDimensionalMgonalPyramidalGenerator.ts
@@ -0,0 +1,20 @@
+import type { MConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
+import {
+  pseudoRisingFactorial,
+  factorialIter,
+} from "../utils/helpersMultidimensionalFigNumbers.js";
+
+export type KDimensionalMgonalPyramidalConfig = MConfigStepConfig & { k: bigint };
+
+export class KDimensionalMgonalPyramidalGenerator
+  implements INumberGeneratorStrategy<KDimensionalMgonalPyramidalConfig>
+{
+  *generate({ step = 1n, m = 4n, k = 4n }: KDimensionalMgonalPyramidalConfig): Generator<bigint> {
+    let delta = 1n;
+    while (true) {
+      yield (pseudoRisingFactorial(delta, k) * ((m - 2n) * delta - m + k + 2n)) / factorialIter(k);
+      delta += step;
+    }
+  }
+}
