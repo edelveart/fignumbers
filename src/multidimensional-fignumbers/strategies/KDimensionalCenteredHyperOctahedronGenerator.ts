a9023ecb131f68fe7c5ca56806d102e3642d57aa
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-21 13:09:37 -0500

    feat(multidimensional-fignumbers/strategies): add KDimensionalCenteredHyperOctahedronGenerator class

---
+++ src/multidimensional-fignumbers/strategies/KDimensionalCenteredHyperOctahedronGenerator.ts
@@ -0,0 +1,29 @@
+import type { KConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
+import { binomialCoeff } from "../utils/helpersMultidimensionalFigNumbers.js";
+
+const exteriorInteriorDoubleSummation = (k: bigint, n: bigint): bigint => {
+  let acc = 0n;
+  for (let j = 1n; j <= n - 1n; j++) {
+    for (let i = 0n; i <= k - 1n; i++) {
+      acc += 2n ** (1n + i) * binomialCoeff(k, 1n + i) * binomialCoeff(j, i);
+    }
+  }
+
+  return 2n * k + acc + 1n;
+};
+
+export type KDimensionalCenteredHyperOctahedronConfig = KConfigStepConfig;
+
+export class KDimensionalCenteredHyperOctahedronGenerator
+  implements INumberGeneratorStrategy<KDimensionalCenteredHyperOctahedronConfig>
+{
+  *generate({ step = 1n, k = 5n }: KDimensionalCenteredHyperOctahedronConfig): Generator<bigint> {
+    let delta = 1n;
+    yield 1n;
+    while (true) {
+      yield exteriorInteriorDoubleSummation(k, delta);
+      delta += step;
+    }
+  }
+}
--- src/multidimensional-fignumbers/strategies/index.ts
+++ src/multidimensional-fignumbers/strategies/index.ts
@@ -1,17 +1,15 @@
 export * from "./PentatopeGenerator.js";
 
 export * from "./KDimensionalHyperTetrahedronGenerator.js";
+export * from "./KDimensionalHyperCubeGenerator.js";
+export * from "./KDimensionalHyperOctahedronGenerator.js";
 
 export * from "./BiquadraticGenerator.js";
-
-export * from "./KDimensionalHyperCubeGenerator.js";
-
-export * from "./KDimensionalHyperOctahedronGenerator.js";
+export * from "./CenteredBiquadraticGenerator.js";
 
 export * from "./HyperOctahedralGenerator.js";
 export * from "./HyperIcosahedralGenerator.js";
 export * from "./HyperDodecahedralGenerator.js";
-
 export * from "./PolyOctahedralGenerator.js";
 
 export * from "./FourDimensionalMgonalPyramidalGenerator.js";
@@ -19,14 +17,12 @@ export * from "./FiveDimensionalMgonalPyramidalGenerator.js";
 export * from "./SixDimensionalMgonalPyramidalGenerator.js";
 
 export * from "./KDimensionalCenteredHyperCubeGenerator.js";
-
-export * from "./CenteredPolytopeGenerator.js";
-
 export * from "./KDimensionalCenteredHyperTetrahedronGenerator.js";
-
-export * from "./CenteredHyperOctahedralGenerator.js";
-export * from "./NexusGenerator.js";
-
-export * from "./CenteredBiquadraticGenerator.js";
+export * from "./KDimensionalCenteredHyperOctahedronGenerator.js";
 
 export * from "./KDimensionalMgonalPyramidalGenerator.js";
+
+export * from "./CenteredPolytopeGenerator.js";
+export * from "./CenteredHyperOctahedralGenerator.js";
+
+export * from "./NexusGenerator.js";
