a9b0b56ef2cf9c909cde7488c315faf74fe4899c
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-22 15:35:16 -0500

    feat(space-fignumbers/strategies): add CenteredMgonalPyramidSpecificGenerator class

--- src/space-fignumbers/strategies/CenteredMgonalPyramidGenerator.ts
+++ src/space-fignumbers/strategies/CenteredMgonalPyramidGenerator.ts
@@ -1,4 +1,4 @@
-import type { MConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type { MConfigStepConfig, StepConfig } from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
 
 export type CenteredMgonalPyramidConfig = MConfigStepConfig;
@@ -12,5 +12,20 @@ export class CenteredMgonalPyramidGenerator
       yield ((m - 1n) * ((delta - 1n) * delta * (2n * delta - 1n))) / 6n + (2n * delta - 1n);
       delta += step;
     }
+  }
+}
+
+export class CenteredMgonalPyramidSpecificGenerator
+  implements INumberGeneratorStrategy<StepConfig>
+{
+  private m: bigint;
+  private baseGenerator = new CenteredMgonalPyramidGenerator();
+
+  constructor(m: bigint) {
+    this.m = m;
+  }
+
+  *generate({ step = 1n }): Generator<bigint> {
+    return yield* this.baseGenerator.generate({ step, m: this.m });
   }
 }
