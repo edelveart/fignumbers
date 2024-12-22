1d9fa5c7aaedb4df0c11cedb75ab121684d0d5cd
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-22 15:35:45 -0500

    feat(space-fignumbers/strategies): add MgonalPrismSpecificGenerator class

--- src/space-fignumbers/strategies/MgonalPrismGenerator.ts
+++ src/space-fignumbers/strategies/MgonalPrismGenerator.ts
@@ -1,14 +1,32 @@
-import type { MConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type {
+  StartConfigMConfigStepConfig,
+  StartConfigStepConfig,
+} from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
 
-export type MgonalPrismConfig = MConfigStepConfig;
+export type MgonalPrismConfig = StartConfigMConfigStepConfig;
 
 export class MgonalPrismGenerator implements INumberGeneratorStrategy<MgonalPrismConfig> {
-  *generate({ step = 1n, m = 3n }: MgonalPrismConfig): Generator<bigint> {
-    let delta = 1n;
+  *generate({ start = 1n, step = 1n, m = 3n }: MgonalPrismConfig): Generator<bigint> {
+    let delta = start;
     while (true) {
       yield (delta * (m * delta ** 2n - m * delta + 2n)) / 2n;
       delta += step;
     }
+  }
+}
+
+export class MgonalPrismSpecificGenerator
+  implements INumberGeneratorStrategy<StartConfigStepConfig>
+{
+  private m: bigint;
+  private baseGenerator = new MgonalPrismGenerator();
+
+  constructor(m: bigint) {
+    this.m = m;
+  }
+
+  *generate({ start = 1n, step = 1n }): Generator<bigint> {
+    return yield* this.baseGenerator.generate({ start, step, m: this.m });
   }
 }
