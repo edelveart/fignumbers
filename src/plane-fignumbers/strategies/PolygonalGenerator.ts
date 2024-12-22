e22a4c835016a2b221362d42a69c22a8fdfc2c6e
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-22 12:34:59 -0500

    feat(plane-fignumbers/strategies): add PolygonalSpecificGenerator class

--- src/plane-fignumbers/strategies/PolygonalGenerator.ts
+++ src/plane-fignumbers/strategies/PolygonalGenerator.ts
@@ -1,4 +1,7 @@
-import type { StartConfigMConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type {
+  StartConfigMConfigStepConfig,
+  StartConfigStepConfig,
+} from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
 
 export type PolygonalConfig = StartConfigMConfigStepConfig;
@@ -10,5 +13,18 @@ export class PolygonalGenerator implements INumberGeneratorStrategy<PolygonalCon
       yield ((m - 2n) * delta ** 2n - (m - 4n) * delta) / 2n;
       delta += step;
     }
+  }
+}
+
+export class PolygonalSpecificGenerator implements INumberGeneratorStrategy<StartConfigStepConfig> {
+  private m: bigint;
+  private baseGenerator = new PolygonalGenerator();
+
+  constructor(m: bigint) {
+    this.m = m;
+  }
+
+  *generate({ start = 1n, step = 1n }): Generator<bigint> {
+    return yield* this.baseGenerator.generate({ start, step, m: this.m });
   }
 }
