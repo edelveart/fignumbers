f8a32dda734d2c5a3ff0f0e76c86556f7f4d3c4b
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-22 15:30:42 -0500

    feat(space-fignumbers/strategies): add PyramidalSpecificGenerator class

--- src/space-fignumbers/strategies/PyramidalGenerator.ts
+++ src/space-fignumbers/strategies/PyramidalGenerator.ts
@@ -1,4 +1,7 @@
-import type { StartConfigMConfigStepConfig } from "../../core/types/ConfigTypes.js";
+import type {
+  StartConfigMConfigStepConfig,
+  StartConfigStepConfig,
+} from "../../core/types/ConfigTypes.js";
 import type { INumberGeneratorStrategy } from "../../core/interfaces/INumberGeneratorStrategy.js";
 
 export type PyramidalConfig = StartConfigMConfigStepConfig;
@@ -10,5 +13,18 @@ export class PyramidalGenerator implements INumberGeneratorStrategy<PyramidalCon
       yield (delta * (delta + 1n) * ((m - 2n) * delta - m + 5n)) / 6n;
       delta += step;
     }
+  }
+}
+
+export class PyramidalSpecificGenerator implements INumberGeneratorStrategy<StartConfigStepConfig> {
+  private m: bigint;
+  private baseGenerator = new PyramidalGenerator();
+
+  constructor(m: bigint) {
+    this.m = m;
+  }
+
+  *generate({ start = 1n, step = 1n }): Generator<bigint> {
+    return yield* this.baseGenerator.generate({ start, step, m: this.m });
   }
 }
