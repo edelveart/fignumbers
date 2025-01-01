dddd31a038c6faea9988a07cc435a1049949acad
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-31 20:36:56 -0500

    refactor: replace static method with instance-based class for PlaneFigNumbers
    - Replaced static generate method with instance method in PlaneFigNumbers
    - Added strategy-based configuration via constructor
    - Improved type safety with generics in class

--- src/plane-fignumbers/context/PlaneFigNumbers.ts
+++ src/plane-fignumbers/context/PlaneFigNumbers.ts
@@ -2,11 +2,14 @@ import { type PlaneConfigMap, PLANE_MAP_STRATEGY } from "../mapped-types/PlaneMa
 import { CONFIG_CONSTANTS } from "../../core/types/configConstants.js";
 import { Validator } from "../../core/validators/Validator.js";
 
-export class PlaneFigNumbers {
-  static generate<KMap extends keyof PlaneConfigMap>(
-    strategyName: KMap,
-    config: PlaneConfigMap[KMap],
-  ): Generator<bigint> {
+export class PlaneFigNumbers<KMapStrategy extends keyof PlaneConfigMap> {
+  private strategy: KMapStrategy;
+
+  constructor(strategy: KMapStrategy) {
+    this.strategy = strategy;
+  }
+
+  generate(config: PlaneConfigMap[KMapStrategy]): Generator<bigint> {
     if (CONFIG_CONSTANTS.step in config) {
       Validator.validateStep(config.step);
     }
@@ -16,7 +19,6 @@ export class PlaneFigNumbers {
     if (CONFIG_CONSTANTS.m in config) {
       Validator.validateMFacets(config.m);
     }
-
-    return PLANE_MAP_STRATEGY[strategyName].generate(config);
+    return PLANE_MAP_STRATEGY[this.strategy].generate(config);
   }
 }
